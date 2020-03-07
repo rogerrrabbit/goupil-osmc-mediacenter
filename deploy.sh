2da730ffd56b58a9047f316341fd6c2012f8d245
rogerrrabbit <mas.vincent@gmail.com>
2020-03-07 11:50:15 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,7 @@ cd $TMP_DIR
 
 systemctl stop mediacenter
 
-echo "[1] Installing moonlight binary"
-echo "deb http://archive.itimmer.nl/raspbian/moonlight stretch main" >> /etc/apt/sources.list
-wget http://archive.itimmer.nl/itimmer.gpg
-apt-key add itimmer.gpg
-apt update
-apt -y dist-upgrade
-apt -y install moonlight-embedded
-
-echo "[2] Installing moonlight service"
+echo "[1] Installing moonlight service"
 wget https://raw.githubusercontent.com/rogerrrabbit/goupil-osmc-mediacenter/master/etc/systemd/system/moonlight.service
 wget https://raw.githubusercontent.com/rogerrrabbit/goupil-osmc-mediacenter/master/moonlight/moonlight.py
 mkdir $MOONLIGHT_SCRIPT_DIR
@@ -29,6 +21,14 @@ cp moonlight.service /etc/systemd/system/
 cp moonlight.py $MOONLIGHT_SCRIPT_DIR
 chmod -R +rx $MOONLIGHT_SCRIPT_DIR
 chmod 644 /etc/systemd/system/moonlight.service
+
+echo "[2] Installing moonlight binary"
+echo "deb http://archive.itimmer.nl/raspbian/moonlight stretch main" >> /etc/apt/sources.list
+wget http://archive.itimmer.nl/itimmer.gpg
+apt-key add itimmer.gpg
+apt update
+apt -y dist-upgrade
+apt -y install moonlight-embedded
 
 echo "Note: you'll have to pair moonlight with the PC, logged as osmc: 'moonlight pair'"
 
@@ -40,15 +40,7 @@ apt update
 apt -y install raspotify
 systemctl enable raspotify.service
 
-echo "[4] Installing retropie"
-apt -y install libfreeimage-dev libfreetype6-dev libcurl4-openssl-dev cmake libvlc-dev libvlccore-dev vlc rapidjson-dev
-wget http://steinerdatenbank.de/software/omxplayer_20180910~7f3faf6~stretch_armhf.deb
-dpkg -i omxplayer_201809107f3faf6stretch_armhf.deb
-git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
-chmod +x RetroPie-Setup/retropie_setup.sh
-RetroPie-Setup/retropie_setup.sh
-
-echo "[5] Installing retropie service"
+echo "[4] Installing retropie service"
 wget https://raw.githubusercontent.com/rogerrrabbit/goupil-osmc-mediacenter/master/etc/systemd/system/retropie.service
 wget https://raw.githubusercontent.com/rogerrrabbit/goupil-osmc-mediacenter/master/retropie/retropie.py
 mkdir $RETROPIE_SCRIPT_DIR
@@ -56,6 +48,14 @@ cp retropie.service /etc/systemd/system/
 cp retropie.py $RETROPIE_SCRIPT_DIR
 chmod -R +rx $RETROPIE_SCRIPT_DIR
 chmod 644 /etc/systemd/system/retropie.service
+
+echo "[5] Installing retropie"
+apt -y install libfreeimage-dev libfreetype6-dev libcurl4-openssl-dev cmake libvlc-dev libvlccore-dev vlc rapidjson-dev
+wget http://steinerdatenbank.de/software/omxplayer_20180910~7f3faf6~stretch_armhf.deb
+dpkg -i omxplayer_201809107f3faf6stretch_armhf.deb
+git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+chmod +x RetroPie-Setup/retropie_setup.sh
+RetroPie-Setup/retropie_setup.sh
 
 cd -
 rm -rf $TMP_DIR
