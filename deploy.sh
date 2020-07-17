3ec98fdd8237ed2c2be8c4ea53f2f7d3e2e96988
rogerrrabbit <mas.vincent@gmail.com>
2020-07-17 23:27:14 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ "${USER}" != "root" ] && echo "Must be run by root" && exit 1
+[ "${USER}" != "osmc" ] && echo "Must be run by user 'osmc'" && exit 1
 
 [ $(cat /etc/debian_version | cut -d. -f1) != 9 ] && echo "Made for raspbian stretch only" && exit 1
 
@@ -11,53 +11,51 @@ RETROPIE_SCRIPT_DIR=/home/osmc/retropie/
 mkdir -p $TMP_DIR
 cd $TMP_DIR
 
-systemctl stop mediacenter
+sudo systemctl stop mediacenter
 
 echo "[1] Installing moonlight service"
 wget https://raw.githubusercontent.com/rogerrrabbit/goupil-osmc-mediacenter/master/etc/systemd/system/moonlight@.service
 wget https://raw.githubusercontent.com/rogerrrabbit/goupil-osmc-mediacenter/master/moonlight/moonlight.py
 wget https://raw.githubusercontent.com/rogerrrabbit/goupil-osmc-mediacenter/master/moonlight/moonlight-steam.py
 mkdir $MOONLIGHT_SCRIPT_DIR
-cp moonlight@.service /etc/systemd/system/
+sudo cp moonlight@.service /etc/systemd/system/
 cp moonlight.py $MOONLIGHT_SCRIPT_DIR
 cp moonlight-steam.py $MOONLIGHT_SCRIPT_DIR
 chmod -R +rx $MOONLIGHT_SCRIPT_DIR
-chmod 644 /etc/systemd/system/moonlight@.service
+sudo chmod 644 /etc/systemd/system/moonlight@.service
 
 echo "[2] Installing moonlight binary"
-echo "deb http://archive.itimmer.nl/raspbian/moonlight stretch main" >> /etc/apt/sources.list
+sudo echo "deb http://archive.itimmer.nl/raspbian/moonlight stretch main" >> /etc/apt/sources.list
 wget http://archive.itimmer.nl/itimmer.gpg
-apt-key add itimmer.gpg
-apt update
-apt -y dist-upgrade
-apt -y install moonlight-embedded
+sudo apt-key add itimmer.gpg
+sudo apt update
+sudo apt -y dist-upgrade
+sudo apt -y install moonlight-embedded
 
 echo "Note: you'll have to pair moonlight with the PC, logged as osmc: 'moonlight pair'"
 
 echo "[3] Installing raspotify"
-apt -y install curl apt-transport-https
-curl -sSL https://dtcooper.github.io/raspotify/key.asc | apt-key add -v -
-echo 'deb https://dtcooper.github.io/raspotify jessie main' | tee /etc/apt/sources.list.d/raspotify.list
-apt update
-apt -y install raspotify
-systemctl enable raspotify.service
+sudo apt -y install curl apt-transport-https
+sudo curl -sSL https://dtcooper.github.io/raspotify/key.asc | apt-key add -v -
+sudo echo 'deb https://dtcooper.github.io/raspotify jessie main' | tee /etc/apt/sources.list.d/raspotify.list
+sudo apt update
+sudo apt -y install raspotify
+sudo systemctl enable raspotify.service
 
-echo "[4] Installing Amazon VOD repository"
-wget https://github.com/Sandmann79/xbmc/releases/download/v1.0.2/repository.sandmann79.plugins-1.0.2.zip
-unzip repository.sandmann79.plugins-1.0.2.zip -d /home/osmc/.kodi/addons/
-systemctl restart mediacenter
-sqlite3 /home/osmc/.kodi/userdata/Database/Addons27.db 'update installed set enabled=1 where addonid=="repository.sandmann79.plugins";'
-systemctl restart mediacenter
-kodi-send --action="InstallAddon(plugin.video.amazon-test)"
-
-echo "[5] Installing retrosmc"
-wget http://steinerdatenbank.de/software/omxplayer_20180910~7f3faf6~stretch_armhf.deb
-dpkg -i omxplayer_201809107f3faf6stretch_armhf.deb
+echo "[4] Installing retrosmc"
 wget https://raw.githubusercontent.com/mcobit/retrosmc/master/install-retrosmc.sh
 chmod +x install-retrosmc.sh
 ./install-retrosmc.sh
 
+echo "[5] Installing Amazon VOD repository"
+wget https://github.com/Sandmann79/xbmc/releases/download/v1.0.2/repository.sandmann79.plugins-1.0.2.zip
+unzip repository.sandmann79.plugins-1.0.2.zip -d /home/osmc/.kodi/addons/
+sudo systemctl restart mediacenter
+sqlite3 /home/osmc/.kodi/userdata/Database/Addons27.db 'update installed set enabled=1 where addonid=="repository.sandmann79.plugins";'
+sudo systemctl restart mediacenter
+kodi-send --action="InstallAddon(plugin.video.amazon-test)"
+
 cd -
-rm -rf $TMP_DIR
+sudo rm -rf $TMP_DIR
 echo "[Done]"
 
